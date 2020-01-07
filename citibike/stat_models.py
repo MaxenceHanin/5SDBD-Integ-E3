96aa9c8700512fc14278049fc79c3217bd7e0b69
Louis Jean <ljean@etud.insa-toulouse.fr>
2020-01-07 15:01:31 +0100

    Statistical model working

--- citibike/stat_models.py
+++ citibike/stat_models.py
@@ -2,23 +2,12 @@ import pandas as pd
 import numpy as np
 import operator
 import collections
+import logging
 
 from sklearn.cluster import KMeans
+from sklearn.model_selection import train_test_split
 import sklearn.metrics as metrics
 
-# https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
-
-df = pd.read_csv("../datasets/JC-201909-citibike-tripdata.csv")
-
-# Create hour and weekday from starttime
-df["date"] = pd.to_datetime(df["starttime"])
-df["hour"] = df["date"].dt.hour
-df["weekday"] = df["date"].dt.weekday
-
-features = df[["hour", "weekday", "start station id"]]
-labels = df[["end station id"]]
-
-print(df.iloc[0])
 
 class StatisticEstimator:
     def __init__(self):
@@ -42,13 +31,65 @@ class StatisticEstimator:
         for i in range(n):
             key = tuple(data.iloc[i])
             props = self.counts[key]
-            val = max(props.iteritems(), key=operator.itemgetter(1))[0]
+            
+            if len(props) != 0:
+                val = max(props.items(), key=operator.itemgetter(1))[0]
+            else:
+                val = -1
+            
             predictions.append(val)
             
         return np.array(predictions)
         
-if __name__ == "__main__":
-    estimator = StatisticEstimator()
-    estimator.fit(features, labels)
 
-    print(len(estimator.counts), estimator.total)
+class CitibikeEstimator:
+    def __init__(self):
+        self.precise_estimator = StatisticEstimator()
+        self.general_estimator = StatisticEstimator()
+        
+    def fit(self, data, targets):
+        self.precise_estimator.fit(data, targets)
+        self.general_estimator.fit(data[["start station id"]], targets)
+    
+    def predict(self, data):
+        precise_preds = self.precise_estimator.predict(data)
+        general_preds = self.general_estimator.predict(data[["start station id"]])
+        # TODO
+        return general_preds
+        
+        
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    
+    # https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+    df = pd.read_csv("../datasets/JC-201909-citibike-tripdata.csv")
+    logging.info("Dataset loaded!")
+
+    # Create hour and weekday from starttime
+    df["date"] = pd.to_datetime(df["starttime"])
+    df["hour"] = df["date"].dt.hour
+    df["weekday"] = df["date"].dt.weekday
+    
+    featuresCols = ["hour", "weekday", "start station id"]
+    labelsCols = ["end station id"]
+    
+    train_df, test_df = train_test_split(df, test_size=0.2)
+
+    train_features = train_df[featuresCols]
+    train_labels = train_df[labelsCols]
+    test_features = test_df[featuresCols]
+    test_labels = test_df[labelsCols]
+    
+    logging.info("Dataset preprocessed!")
+    
+    # Test estimator
+    estimator = StatisticEstimator()
+    estimator.fit(train_features, train_labels)
+    
+    logging.info("Training done!")
+    
+    test_predictions = estimator.predict(test_features)
+    accuracy = metrics.accuracy_score(test_labels.values.reshape(-1), test_predictions)
+    
+    print("Precision score = %f" % (accuracy,))
+    
