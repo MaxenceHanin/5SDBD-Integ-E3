373f5674a28fe3e39ba458cec33282b6f7fbe2cd
Louis Jean <ljean@etud.insa-toulouse.fr>
2020-01-08 10:46:24 +0100

    Enhance the random forest spark script (add crossvalidation and test is now working)

--- citibike/spark_random_forest.py
+++ citibike/spark_random_forest.py
@@ -2,6 +2,7 @@ from pyspark.ml import Pipeline
 from pyspark.ml.classification import RandomForestClassifier
 from pyspark.ml.feature import StringIndexer, VectorIndexer, VectorAssembler
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
 
 from pyspark.sql import SparkSession
 from pyspark.sql.types import ArrayType, DoubleType
@@ -40,7 +41,8 @@ data = data.withColumn("month", month("starttime"))\
             .withColumn("weekday", dayofweek("starttime"))
 
 # Split the data into training and test sets (30% held out for testing)
-(trainingData, testData) = data.randomSplit([0.7, 0.3])
+(trainingData, testData) = data.randomSplit([0.8, 0.2])
+print("Training on %d trips, Testing on %d trips" % (trainingData.count(), testData.count()))
 
 
 #### TRAINING
@@ -51,7 +53,7 @@ stages = []
 strIndexer = StringIndexer(inputCol="start station id", outputCol = "start station index")
 stages += [strIndexer]
 
-endIndexer = StringIndexer(inputCol="end station id", outputCol = "end station index")
+endIndexer = StringIndexer(inputCol="end station id", outputCol = "end station index", handleInvalid="keep")
 stages += [endIndexer]
 
 featuresCols = ["month", "hour", "weekday", "start station index"]
@@ -60,27 +62,36 @@ stages += [assembler]
 
 # Train a random forest model.
 rfc = RandomForestClassifier(labelCol="end station index", featuresCol="features", numTrees=100, maxBins=64, maxDepth=7)
+paramGrid = ParamGridBuilder().addGrid(rfc.numTrees, [50, 100, 200]).addGrid(rfc.maxDepth, [6, 7, 9, 11]).build()
 stages += [rfc]
 
 pipeline = Pipeline(stages=stages)
 
+crossval = CrossValidator(estimator=pipeline,
+                          estimatorParamMaps=paramGrid,
+                          evaluator=MulticlassClassificationEvaluator(labelCol="end station index", predictionCol="prediction", metricName="accuracy"),
+                          numFolds=3)
+
 # Train model.
+# <!> currently we do not use the CrossValidator.
+# To use CrossValidator change the line to:
+# model = crossval.fit(trainingData)
 model = pipeline.fit(trainingData)
 
 # Make predictions.
 predictions = model.transform(testData)
 
 # Select example rows to display.
-predictions.select("prediction", "features", "end station index").show(5)
+predictions = predictions.select("prediction", "features", "end station index")
+predictions.show(5)
 
 
 #### EVALUATION
 
 # Select (prediction, true label) and compute test error
-evaluator = MulticlassClassificationEvaluator(
-    labelCol="end station index", predictionCol="prediction", metricName="accuracy")
+evaluator = MulticlassClassificationEvaluator(labelCol="end station index", predictionCol="prediction", metricName="accuracy")
 accuracy = evaluator.evaluate(predictions)
-print("Test Error = %g" % (1.0 - accuracy))
+print("Test Accuracy = %g" % (accuracy))
 
 rfcModel = model.stages[-1]
 print(rfcModel)  # summary only
