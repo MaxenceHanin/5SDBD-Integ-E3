49e1324321ff5603a1b07a627d8d6e95e84d612d
Louis Jean <ljean@etud.insa-toulouse.fr>
2020-01-20 17:47:18 +0100

    Add functions to User Interface (including call to the prediction service)

--- services/Storage/README.md
+++ services/Storage/README.md
@@ -2,8 +2,24 @@
 
 - ["httpservlet clas not found"](https://howtodoinjava.com/eclipse/solved-the-superclass-javax-servlet-http-httpservlet-was-not-found-on-the-java-build-path-in-eclipse/)
 
+- Set the context roots: 
+	- **Predictions** : 5sdbd-integ-e3/predictions/
+	- **Storage** : 5sdbd-integ-e3/storage/
+	- **UserInterface** : citibike-app
+
 ## Help and tutorials
 
 - [Test REST API with curl](https://www.baeldung.com/curl-rest)
 
-curl -v "http://localhost:8080//5sdbd-integ-e3.fr/storage/data/stations"
+```bash
+# Get the list of all the stations
+curl -v "http://localhost:8080//5sdbd-integ-e3/storage/data/stations"
+# Get all stations around one station
+curl -v "http://localhost:8080//5sdbd-integ-e3/storage/data/stations/320/"
+# Prediction example
+curl -v "http://localhost:8080/5sdbd-integ-e3/predictions/predict/next_station/32?hour=5&weekday=2&month=12&age=5&gender=1"
+```
+
+- **User interface**: 
+
+go to the address `http://localhost:8080/citibike-app/`
--- services/UserInterface/WebContent/js/script.js
+++ services/UserInterface/WebContent/js/script.js
@@ -1,7 +1,5 @@
 window.onload = function() {
     
-    predict_next_station = document.getElementById("predict-next-station");
-    
     function sendGet(address, callback, err) {
     	var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
@@ -19,41 +17,78 @@ window.onload = function() {
     // Key = station name, value = id
     var stationList = {};
     
-    $("#date").datepicker();
+    $(".datepicker").datepicker();
     
-    sendGet("http://localhost:8080/5sdbd-integ-e3/storage/data/stations", function(responseText) {
-    	response = JSON.parse(responseText);
-        resultStr = "";
-        
-        for (key in response) {
+    function updateStationDD(response, dropdown) {
+    	for (key in response) {
             val = response[key];
-        	stationList[val["stationName"]] = parseInt(val["idS"]);
-        	
-        	dropdown = document.getElementById("station");
-        	
         	var option = document.createElement("option");
         	option.text = val["stationName"];
         	dropdown.add(option);
         }
-    });
+    }
     
-    predict_next_station.onsubmit = function() {
-        formData = new FormData(predict_next_station);
+    sendGet("http://localhost:8080/5sdbd-integ-e3/storage/data/stations", function(responseText) {
+    	response = JSON.parse(responseText);
+
+    	for (key in response) {
+            val = response[key];
+        	stationList[val["stationName"]] = parseInt(val["idS"]);
+        }
+
+    	updateStationDD(response, document.getElementById("station1"));
+    	updateStationDD(response, document.getElementById("station2"));
+    });
+
+    get_station_info = $("#get-station-info");
+    get_station_info.submit(function(e) {
+        e.preventDefault();
+        
+        formData = new FormData(get_station_info.get(0));
         station_name = formData.get("station");
         station_id = stationList[station_name];
-        date = Date.parse(formData.get("date"));
         
         if (!station_id) {
-            document.getElementById("result").innerHTML = "Bad station id";
+            document.getElementById("result-station-info").innerHTML = "Bad station id";
         }
         
         sendGet("http://localhost:8080/5sdbd-integ-e3/storage/data/stations/" + station_id, function(responseText) {
         	val = JSON.parse(responseText);
             // Returns station id, name, latitude and longitude
             resultStr = val["idS"] + ": " + val["stationName"] + " (" + val["latitude"] + ", " + val["longitude"] + ")<br/>";
-            document.getElementById("result").innerHTML = resultStr;
+            document.getElementById("result-station-info").innerHTML = resultStr;
         });
         
         return false;
-    };
+    });
+    
+    predict_next_station = $("#predict-next-station");
+    predict_next_station.submit(function(e) {
+    	e.preventDefault();
+    	
+        formData = new FormData(predict_next_station.get(0));
+        station_name = formData.get("station");
+        station_id = stationList[station_name];
+        date = new Date(formData.get("date"));
+        hour = parseInt(formData.get("hour"));
+        
+        if (!station_id) {
+            document.getElementById("result-next-station").innerHTML = "Bad station id";
+        }
+        
+        address = "http://localhost:8080/5sdbd-integ-e3/predictions/predict/next_station/"
+    		+ station_id + "?hour=" + hour + "&weekday=" + date.getDay() + "&month=" + (date.getMonth() + 1)
+    		+ "&age=0&gender=0";
+        console.log(address);
+        
+        sendGet(address, function(responseText) {
+        	val = JSON.parse(responseText);
+            // Returns station id, name, latitude and longitude
+            resultStr = val["idS"] + ": " + val["stationName"] + " (" + val["latitude"] + ", " + val["longitude"] + ")<br/>";
+            document.getElementById("result-next-station").innerHTML = resultStr;
+        });
+        
+        return false;
+    });
+    
 }
