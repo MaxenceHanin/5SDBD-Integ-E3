7051c25515b7006eb609607b695cd05c026b9f4f
tangid <33022427+tangid@users.noreply.github.com>
2020-01-21 18:19:46 +0100

    Add files via upload
    Upload to MongoDB

--- citibike/RandomForest.py
+++ citibike/RandomForest.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[22]:
-
-
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from time import time
+from pymongo import MongoClient
 
 import pickle
-import pandas as pd
 import csv
 import os
 import numpy as np
-
+import pandas as pd
 
 
 # In[25]:
@@ -35,94 +32,25 @@ file2.drop(labels="long_o", axis=1, inplace=True)
 file2.drop(labels="lat_d", axis=1, inplace=True)
 file2.drop(labels="long_d\t", axis=1, inplace=True)
 
-result = pd.merge(file2, file1, how='outer', left_on='id_o', right_on='id').dropna()
-
-y = result["cluster"][0:100000]
+result = pd.merge(file2, file1, how='outer', left_on='id_d', right_on='id').dropna()
+taille_ech = 20000
+y = result["cluster"][0:taille_ech]
 result.drop(labels="id_d", axis=1, inplace=(True))
 result.drop(labels="id", axis=1, inplace=(True))
 result.drop(labels="cluster", axis=1, inplace=(True))
 
-x = result[0:100000]
-
-
-# In[23]:
-
+x = result[0:taille_ech]
 
 test_size=0.3
 X_train, X_test, Y_train, Y_test = train_test_split(x, y, test_size=test_size)
 
-
-# In[24]:
-
-
 #Algo Random forest
-min_sample = [0.0001,0.001,0.01,0.1,1,10]
-estimator = [10,50,100,200]
-for i in min_sample:
-    for j in estimator:
-        print("i : ", i)
-        begin = time()
-        model = RandomForestClassifier(min_samples_leaf=i, random_state=0, n_estimators = j)
-        model.fit(X_train, Y_train)
-        end = time()
-        print("Contexte : Min_Samples_leaf : " + str(i) + " Estimator : " +str(j))
-        print("Score : ", model.score(X_test, Y_test))
-        print("Temps d'execution : ", end-begin)
-
-
-# In[25]:
-
 
 model = RandomForestClassifier(min_samples_leaf=1, random_state=0, n_estimators = 10)
 model.fit(X_train, Y_train)
 
 
-# In[20]:
-
-
-print("Score : ", model.score(X_test, Y_test))
-print("Temps d'execution : ", end-begin)
-
-
-# In[10]:
-
-
-print(result)
-
-
-# In[15]:
-
-
-pd.merge(file2, file1, how='outer', left_on='id_o', right_on='id').dropna()[200000:200001]
-
-
-# In[16]:
-
-
-result[200000:200001]
-
-
-# In[14]:
-
-
-model.predict(result[200000:200001])
-
-
-# In[26]:
-
-
-#Sauvegarde modele 
-filename = 'model_random_forest.sav'
-pickle.dump(model, open(filename, 'wb'))
-
-#chargement model
-#loaded_model = pickle.load(open(filename, 'rb'))
-
-
-# In[4]:
-
-
-def save_model_to_db(model, client, db, dbconnection, model_name):
+def save_model_to_db(model, client, db, dbconnection, model_name, id_model):
     pickled_model = pickle.dumps(model)
     
     myclient = MongoClient(client)
@@ -130,31 +58,10 @@ def save_model_to_db(model, client, db, dbconnection, model_name):
     mydb = myclient[db]
     
     mycon = mydb[dbconnection]
-    info = mycon.insert_one({model_name: pickled_model, 'name': model_name, 'created_time':time()})
+    info = mycon.insert_one({'idS': id_model, 'model': pickled_model, 'name': model_name})
     print(info.inserted_id, ' saved with this id successfully!')
-    
-    details = {
-        'idM': 14,
-        'model':model_name,
-        'name': model_name
-    }
-    
-    return (details)
-
-
-# In[5]:
-
-
-import panda as pd
 
 client = "mongodb+srv://MaxenceHanin:Revente21*@predmodel-4pwxr.mongodb.net/test?retryWrites=true&w=majority"
 db = "CitiBike"
 dbconnection = 'Models'
-save_model_to_db(model, client, db, dbconnection, model_name="test_RandomForest")
-
-
-# In[ ]:
-
-
-
-
+save_model_to_db(model, client, db, dbconnection, model_name="Random_Forest_Model", id_model=70)
--- citibike/gradientboosting.py
+++ citibike/gradientboosting.py
@@ -1,3 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
 print("import lib")
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
@@ -5,37 +11,54 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.ensemble import GradientBoostingClassifier
 from xgboost import XGBClassifier
+from pymongo import MongoClient
+
 import xgboost
 from time import time
+import pickle
+import csv
+import os
+import numpy as np
+import pandas as pd
+
+
+# In[24]:
+
 
 print("Import data")
 #import data
-full_data = pd.read_csv("res_201801",sep=",", names=["id_o", "id_d", "dur", "dis", "h", "w", "m", "typ", "gen", "age", "lat_o", "long_o", "lat_d", "long_d"])
-full_data.drop(labels="dur", axis=1, inplace=True)
-full_data.drop(labels="dis", axis=1, inplace=True)
-full_data.drop(labels="lat_d", axis=1, inplace=True)
-full_data.drop(labels="long_d", axis=1, inplace=True)
-full_data.drop(labels="typ", axis=1, inplace=True)
+file = pd.read_csv('clustered_stations.csv', sep=",")
+file.drop(labels="t", axis=1, inplace=True)
+file.drop(labels="name", axis=1, inplace=True)
+file.drop(labels="docks", axis=1, inplace=True)
+file.drop(labels="latitude", axis=1, inplace=True)
+file.drop(labels="longitude", axis=1, inplace=True)
+file.drop(labels="address", axis=1, inplace=True)
+file.drop(labels="color", axis=1, inplace=True)
 
+file2 = pd.read_csv('res_201801.csv', sep=",")
+file2.drop(labels="duration", axis=1, inplace=True)
+file2.drop(labels="distance", axis=1, inplace=True)
+file2.drop(labels="lat_o", axis=1, inplace=True)
+file2.drop(labels="long_o", axis=1, inplace=True)
+file2.drop(labels="lat_d", axis=1, inplace=True)
+file2.drop(labels="long_d\t", axis=1, inplace=True)
 
+result = pd.merge(file2, file, how='outer', left_on='id_d', right_on='id').dropna()
 
+y = result["cluster"][1:500000]
+result.drop(labels="id_d", axis=1, inplace=(True))
+result.drop(labels="id", axis=1, inplace=(True))
+result.drop(labels="cluster", axis=1, inplace=(True))
 
-
-y_train = full_data["id_d"].values[1:20000]
-full_data.drop(labels="id_d", axis=1, inplace=True)
-
-#split data
-X_train = full_data.values[1:20000]
-
-scaler = MinMaxScaler()
-X_train = scaler.fit_transform(X_train)
+x = result[1:500000]
 
 # seed used by the random number generator
 state = 12  
 test_size = 0.30  
 
 #Split arrays or matrices into random train and test subsets
-X_train, X_val, y_train, y_val = train_test_split(X_train, y_train,  
+X_train, X_val, y_train, y_val = train_test_split(x, y,  
     test_size=test_size, random_state=state)
 
 #try setting different learning rates, compare the performance of the classifier's performance at different learning rates
@@ -57,7 +80,28 @@ xgb_clf = XGBClassifier()
 xgb_clf.fit(X_train, y_train)
 end = time()
 score = xgb_clf.score(X_val, y_val)
-print("time :")
-print(end-begin)
+print("time :", end-begin)
 print(score)
+
+
+def save_model_to_db(model, client, db, dbconnection, model_name, id_model):
+    pickled_model = pickle.dumps(model)
+    
+    myclient = MongoClient(client)
+    
+    mydb = myclient[db]
+    
+    mycon = mydb[dbconnection]
+    info = mycon.insert_one({'idS': id_model, 'model': pickled_model, 'name': model_name})
+    print(info.inserted_id, ' saved with this id successfully!')
+    
+
+
+import pandas as pd
+
+client = "mongodb+srv://MaxenceHanin:Revente21*@predmodel-4pwxr.mongodb.net/test?retryWrites=true&w=majority"
+db = "CitiBike"
+dbconnection = 'Models'
+save_model_to_db(xgb_clf, client, db, dbconnection, model_name="Grad_Boost_Model", id_model=68)
+
 
