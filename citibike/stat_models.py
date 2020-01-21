9bb1882eed580feb46eb6a34521ef5912b43cca8
Louis Jean <ljean@etud.insa-toulouse.fr>
2020-01-21 16:30:23 +0100

    Add statistical model for clusters

--- citibike/stat_models.py
+++ citibike/stat_models.py
@@ -42,6 +42,7 @@ class StatisticEstimator:
         return np.array(predictions)
         
 
+# This class is totally unfinished and unusable
 class CitibikeEstimator:
     def __init__(self):
         self.precise_estimator = StatisticEstimator()
@@ -56,15 +57,51 @@ class CitibikeEstimator:
         general_preds = self.general_estimator.predict(data[["start station id"]])
         # TODO
         return general_preds
-        
-        
-if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO)
-    
-    # https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
-    df = pd.read_csv("../datasets/JC-201909-citibike-tripdata.csv")
-    logging.info("Dataset loaded!")
 
+
+### Preprocessing
+
+def add_time(df):
+    # Create hour and weekday from starttime
+    df["date"] = pd.to_datetime(df["starttime"])
+    df["hour"] = df["date"].dt.hour
+    df["weekday"] = df["date"].dt.weekday
+
+
+def with_clusters(data):
+    clusters = pd.read_csv("../datasets/clustered_stations.csv")
+    data = pd.merge(data, clusters[["id", "cluster"]], left_on="start station id", right_on="id")
+    data = pd.merge(data, clusters[["id", "cluster"]], left_on="end station id", right_on="id", suffixes=("", " end"))
+    return data
+
+
+### GENERAL TEST METHODS
+
+def test_estimator(df, featuresCols, labelsCols):
+    train_df, test_df = train_test_split(df, test_size=0.2)
+
+    train_features = train_df[featuresCols]
+    train_labels = train_df[labelsCols]
+    test_features = test_df[featuresCols]
+    test_labels = test_df[labelsCols]
+
+    logging.info("Dataset preprocessed!")
+
+    # Test estimator
+    estimator = StatisticEstimator()
+    estimator.fit(train_features, train_labels)
+
+    logging.info("Training done!")
+
+    test_predictions = estimator.predict(test_features)
+    accuracy = metrics.accuracy_score(test_labels.values.reshape(-1), test_predictions)
+
+    print("Precision score = %f" % (accuracy,))
+
+
+### TESTS
+
+def test_next_station(df):
     # Create hour and weekday from starttime
     df["date"] = pd.to_datetime(df["starttime"])
     df["hour"] = df["date"].dt.hour
@@ -73,23 +110,35 @@ if __name__ == "__main__":
     featuresCols = ["hour", "weekday", "start station id"]
     labelsCols = ["end station id"]
     
-    train_df, test_df = train_test_split(df, test_size=0.2)
+    test_estimator(df, featuresCols, labelCols)
 
-    train_features = train_df[featuresCols]
-    train_labels = train_df[labelsCols]
-    test_features = test_df[featuresCols]
-    test_labels = test_df[labelsCols]
+
+def test_clusters(data):
+    data = with_clusters(data)
     
-    logging.info("Dataset preprocessed!")
+    labels = data["cluster"]
+    predictions = data["cluster end"]
     
-    # Test estimator
-    estimator = StatisticEstimator()
-    estimator.fit(train_features, train_labels)
-    
-    logging.info("Training done!")
-    
-    test_predictions = estimator.predict(test_features)
-    accuracy = metrics.accuracy_score(test_labels.values.reshape(-1), test_predictions)
+    accuracy = metrics.accuracy_score(labels.values.reshape(-1), predictions.values.reshape(-1))
     
     print("Precision score = %f" % (accuracy,))
+
+def test_id_to_clusters(data):
+    add_time(data)
+    data = with_clusters(data)
+    
+    featuresCols = ["start station id"]
+    labelsCols = ["cluster end"]
+    
+    test_estimator(data, featuresCols, labelsCols)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    
+    # https://stackoverflow.com/questions/268272/getting-key-with-maximum-value-in-dictionary
+    df = pd.read_csv("../datasets/201801.csv")
+    logging.info("Dataset loaded!")
+
+    test_id_to_clusters(df)
     
